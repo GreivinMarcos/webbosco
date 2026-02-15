819a2db23b49c1a6663bbecd828468fa0103b1fe
GREIVIN MARCOS <gmarcos20@uhispano.ac.cr>
2026-02-15 11:10:18 -0600

    Arreglo contraseña y logo inicio

--- recuperar.php
+++ recuperar.php
@@ -19,7 +19,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($resultado->num_rows > 0) {
         $token = bin2hex(random_bytes(32));
-        $url_recuperacion = "http://localhost/Tienda%20e-commerce%20BoscoBox/restablecer.php?token=$token";
+        $url_recuperacion = "https://boscoboxfitnesscenter-hwa4dkbrdwcbf7dq.canadacentral-01.azurewebsites.net/restablecer.php?token=$token";
+        /*"http://localhost/Tienda%20e-commerce%20BoscoBox/restablecer.php?token=$token";*/
 
         $update = $conn->prepare("UPDATE usuarios SET token = ? WHERE correo = ?");
         $update->bind_param("ss", $token, $correo);
